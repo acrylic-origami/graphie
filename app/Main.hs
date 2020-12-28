82a9dbb8053a0b84fe29b333d850c0e0da8ae93d
Derek Lam <dc.lam@mail.utoronto.ca>
2020-12-27 20:00:56 -0800

    Connect AST nodes to children + named nodes via trigger
    The name trigger is a bit coarse, I should namespace my `name` field
    or restrict it to the labels that are associated with graphie.
    
    Still, this is the first incarnation of the schema that seems to have
    most of what we want, so development will start on the frontend now.

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,10 @@ import qualified Data.ByteString.UTF8 as UB
 import qualified Data.ByteString.Lazy.UTF8 as UBL
 import qualified Data.Aeson as Aeson
 
+import Data.Char ( toLower )
 import Control.Arrow ( (***), (&&&), first, second )
-import Control.Monad ( void )
+import Control.Exception ( Exception(..), throw )
+import Control.Monad ( void, zipWithM )
 import Data.Data ( showConstr, Data(..) )
 import Data.Foldable ( foldrM )
 import System.Environment ( getArgs )
@@ -46,8 +48,8 @@ import Lib
 
 import Debug.Trace ( trace )
 
-querya_ = Bolt.query_ -- . tracethru
-querya = Bolt.query -- . tracethru
+querya_ = Bolt.query_ . tracethru
+querya = Bolt.query . tracethru
 
 tracethru :: Show a => a -> a
 tracethru s = trace (show s) s
@@ -76,22 +78,28 @@ span2props pre sp = [
   ]
 
 scope2props pre scope =
-  ((pre `T.append` "scope") =: showConstr (toConstr scope))
+  ((pre `T.append` "scope") =: datactor2text scope)
   : (case scope of
     LocalScope sp -> span2props (pre `T.append` "scope_") sp
     _ -> [])
 
-ast2cypher :: Bolt.Pipe -> FilePath -> HieAST TypeIndex -> Bolt.BoltActionT IO Int -- IO ()
-ast2cypher neop path (Node {..}) = do
+recs2id recs ident =
+  let ident' = "id(" `T.append` ident `T.append` ")"
+  in case recs of
+    rec:_ -> rec `Bolt.at` ident'
+    _ -> throw $ Bolt.RecordHasNoKey ident'
+
+ast2cypher :: Bolt.Pipe -> FilePath -> FilePath -> HieAST TypeIndex -> Bolt.BoltActionT IO Int -- IO ()
+ast2cypher neop hs_path hie_path (Node {..}) = do
     let (ann_ctors, ann_tys) = both (map unpackFS) $ unzip $ S.toList $ nodeAnnotations nodeInfo -- S.toList $ S.map (("annotations" =:) . T.pack . UBL.toString . Aeson.encode . both unpackFS)
         -- (sp_fn, sp_l0, sp_ch0, sp_lf, sp_chf) = flat_realsrcspan nodeSpan
         sp_props = span2props "" nodeSpan
-    ((!! "id(n)") -> I this_id) <- querya $ formQuery $ do -- TODO URGENT this pattern match is dangerous; make into exception to rollback transaction upon pat match failure
+    recs <- querya $ formQuery $ do -- TODO URGENT this pattern match is dangerous; make into exception to rollback transaction upon pat match failure
       mergeF [PS $ P $ (#n .: "GraphieAST" .# (("ann_ctors" =: ann_ctors) : ("ann_tys" =: ann_tys) : sp_props))]
       _ <- mapM (\(idx, ty) -> do
           let tynode = withIdentifier (T.pack $ "ty" ++ show idx) defN
           withF ["n"]
-          matchF [PS $ P $ tynode .: "GraphieTy" .# ["idx" =: ty]]
+          matchF [PS $ P $ tynode .: "GraphieTy" .# ["idx" =: ty, "path" =: T.pack hie_path]]
           mergeF [PS $ (#n -: (defR .: "GRAPHIE_AST2TY" .# ["pos" =: idx]) :!->: tynode)]
         ) (zip ([0..] :: [Int]) $ nodeType nodeInfo)
       _ <- mapM (\(idx, (ident, (IdentifierDetails {..}))) -> do
@@ -110,18 +118,19 @@ ast2cypher neop path (Node {..}) = do
             Just ty -> do
               let ident_ty_node = withIdentifier (T.pack $ "ty_" ++ ident_ident) defN
               withF ["n", T.pack ident_ident]
-              matchF [PS $ P $ ident_node .: "GraphieTy" .# ["idx" =: ty]]
+              matchF [PS $ P $ ident_ty_node .: "GraphieTy" .# ["idx" =: ty, "path" =: T.pack hie_path]]
               mergeF [PS $ ident_node -: (defR .: "GRAPHIE_IDENT2TY") :!->: ident_ty_node]
             Nothing -> pure ()
         ) (zip [0..] $ M.assocs (nodeIdentifiers nodeInfo))
       returnF ["id(n)"] -- hard for me to overstate how important this is
-      
-    next_id <- mapM (ast2cypher neop path) nodeChildren
-    _ <- querya $ formQuery $ do
+    
+    this_id <- recs2id recs "n"
+    next_ids <- mapM (ast2cypher neop hs_path hie_path) nodeChildren
+    _ <- (flip (`zipWithM` ([0..] :: [Int])) next_ids) $ \pos next_id -> querya $ formQuery $ do
       matchF [PS $ P #n]
       matchF [PS $ P #m]
-      whereF $ (ID "n" this_id) :&&: (ID "m" this_id)
-      mergeF [PS $ #n -: (defR .: "GRAPHIE_AST2AST") :!->: #m]
+      whereF $ (C $ ID "n" this_id) :&&: (C $ ID "m" next_id)
+      mergeF [PS $ #n -: (defR .: "GRAPHIE_AST2AST" .# ["pos" =: pos]) :!->: #m]
       
     return this_id
     
@@ -131,7 +140,7 @@ ast2cypher neop path (Node {..}) = do
         .: "GraphieCtx"
         .# (
             ("con" =: T.pack con)
-            : ("path" =: T.pack path)
+            : ("path" =: T.pack hie_path)
             : props
           )
       ]
@@ -151,8 +160,8 @@ ast2cypher neop path (Node {..}) = do
         (case m_bindspan of
             Just sp -> span2props "bind_" sp
             Nothing -> [])
-        ++ scope2props "" patscope
-        ++ scope2props "" outscope
+        ++ scope2props "pat_" patscope
+        ++ scope2props "out_" outscope
       ClassTyDecl m_bindspan -> mk_ctx_query node "ClassTyDecl" $ case m_bindspan of
         Just sp -> span2props "bind_" sp
         Nothing -> []
@@ -175,6 +184,9 @@ ast2cypher neop path (Node {..}) = do
 uniquable2text :: Uniquable s => s -> Text
 uniquable2text = T.pack . show . getUnique
 
+datactor2text :: Data d => d -> Text
+datactor2text = T.pack . showConstr . toConstr 
+
 types2cypher :: Foldable t => FilePath -> t (TypeIndex, HieTypeFlat) -> Bolt.BoltActionT IO ()
 types2cypher path tys = do
   -- insert nodes first
@@ -182,7 +194,7 @@ types2cypher path tys = do
       HTyVarTy (uniquable2text -> name) -> mk_ty_query idx "TyVarTy" ["name" =: name]
       HAppTy _t0 _targs -> mk_ty_query idx "AppTy" []
       HTyConApp (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) }) _targs -> mk_ty_query idx "TyConApp" ["name" =: name]
-      HForAllTy _ _ -> pure () -- TODO
+      HForAllTy _ _ -> mk_ty_query idx "ForAllTy" []
       HFunTy tyarg tyret -> mk_ty_query idx "FunTy" []
       HQualTy tyctx tyret -> mk_ty_query idx "FunTy" []
       HLitTy tylit -> mk_ty_query idx "HLitTy" [
@@ -198,7 +210,7 @@ types2cypher path tys = do
   foldrM (\(idx, ty) () -> case ty of
       HAppTy t0 (HieArgs tn) -> mk_args_query t0 tn
       HTyConApp _ (HieArgs tn) -> mk_args_query idx tn -- (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) })
-      HForAllTy _ _ -> pure () -- TODO
+      HForAllTy ((name, ty0), argflag) ty1 -> mk_ty_rel ty0 ty1 ["name" =: uniquable2text name, "argflag" =: datactor2text argflag]
       HFunTy tyarg tyret -> do
         mk_ty_rel idx tyarg ["pos" =: (0 :: Int)]
         mk_ty_rel idx tyret ["pos" =: (1 :: Int)]
@@ -226,13 +238,17 @@ types2cypher path tys = do
         PS $ P $ #b .: "GraphieTy"
                     .# ["path" =: T.pack path, "idx" =: tyb]
         ]
-      mergeF [PS $ (#a -: (defR .: "GRAPHIE_TY_REL" .# relprops) :!->: (#b))]
+      mergeF [PS $ (#a -: (defR .: "GRAPHIE_TY_REL" .# (("path" =: T.pack path) : relprops)) :!->: (#b))]
 
 hiefile_props :: HieFile -> [(Text, Bolt.Value)]
 hiefile_props (HieFile {..}) = [
     "hs_file" =: T.pack hie_hs_file
     , "hie_module" =: uniquable2text hie_module
   ]
+
+-- TODO sanitize
+mk_index_query label prop = querya $ T.pack $
+  "CREATE INDEX ON :" ++ label ++ "(" ++ prop ++ ")"
 
 main :: IO ()
 main = do
@@ -243,18 +259,47 @@ main = do
       Bolt.password = "postgres"
       -- secure = True
     }
+  Bolt.run neop $ do
+    const (pure ()) $ querya $
+      (`T.append` "\", { phase: \"afterAsync\" })") $ T.append "CALL apoc.trigger.add(\"graphie_cliquify_names\",\"" $ T.replace "\"" "\\\"" $ formQuery $ do
+        matchF [PS $ P $ #a]
+        matchF [PS $ P $ #b]
+        whereF $ (C $ TC $ "a.name = b.name") :&&: (C $ TC $ "a <> b")
+        mergeF [PS $ #a -: (defR .: "GRAPHIE_NAME2NAME") :!-: #b]
+    let idxs = [
+            ("GraphieTy", ["con", "path", "idx", "name", "value"])
+            , ("GraphieIdent", ["con", "name"])
+            , ("GraphieCtx", ["con", "path", "bind_spfn", "bind_sp_l0", "bind_sp_ch0", "bind_sp_lf", "bind_sp_chf", "decltycon", "ctxtycon"])
+            , ("GraphieHieFile", ["hs_file", "hie_module"])
+          ]
+    mapM (\(label, props) -> mapM (mk_index_query label) props) idxs
+    
+  Bolt.run neop $ querya $ formQuery $ do
+    matchF [PS $ P #n]
+    whereF $ C $ TC "any(l IN labels(n) WHERE l=~'Graphie.*')"
+    detachDeleteF ["n"]
   dhies <- getArgs -- TODO switch to glob-based
   fhies <- fmap concat $ mapM getHieFilesIn $ dhies
   _ <- foldrM (\path nc' -> do
       (hie_file_result -> hie, nc'') <- readHieFile nc' path
-      Bolt.run neop $ Bolt.transact $ do
+      putStrLn path
+      Bolt.run neop $ do -- Bolt.transact $ z
         -- base file
-        querya $ formQuery $ mergeF [PS $ P $ defN .: "GraphieHieFile" .# hiefile_props hie]
+        recs <- querya $ formQuery $ do
+          mergeF [PS $ P $ #n .: "GraphieHieFile" .# hiefile_props hie]
+          returnF ["id(n)"]
+        hie_file_id <- recs2id recs "n"
         -- types
         types2cypher path (A.assocs $ hie_types hie)
         -- TODO exported names
         -- i.e. exports2cypher (hie_exports hie)
-        foldrM (\(fs_name, ast) () -> ast2cypher neop (unpackFS fs_name) ast) () (M.assocs $ getAsts $ hie_asts hie)
+        -- ASTs
+        ast_ids <- mapM (\(fs_name, ast) -> ast2cypher neop (unpackFS fs_name) path ast) (M.assocs $ getAsts $ hie_asts hie)
+        (`mapM` ast_ids) $ \ast_id -> querya $ formQuery $ do
+          matchF [PS $ P $ #n .: "GraphieHieFile"]
+          matchF [PS $ P $ #m .: "GraphieAST"]
+          whereF $ (C $ ID "n" hie_file_id) :&&: (C $ ID "m" ast_id)
+          mergeF [PS $ #n -: (defR .: "GRAPHIE_FILE2AST") :!->: #m]
       return nc''
     ) nc fhies
   pure ()
