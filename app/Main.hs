afca3595f0b904b0ec43ab37f4c3a30d983233c2
Derek Lam <dc.lam@mail.utoronto.ca>
2020-12-27 16:35:17 -0800

    Working sketch of HIE->neo4j direction
    Shoved most single-file AST information into Neo4J, including the space-
    efficient type representation in HIE.
    
    The codebase is a little messy right now because of the ad-hoc treatment
    of BoltActionT vs. Free Expr () -- between the query action and the
    DSL monad resp. It's surprising sometimes where the query builder needs
    to cross boundaries, for example when preserving a node identifier (the
    temporary name given to nodes in queries) I need to encapsulate all the
    dependents into the same query. This is currently a bit dangerous given
    the maximum default query length is ~50Kchar, given I shove all the
    properties of an AST node in a single query so I can point the node to
    all its non-flat/normalized properties (e.g. NodeContext information).
    The more reasonable strategy may be to return IDs from node-forming
    queries and string them along BoltActionT, although I don't love the
    error-prone pattern matching to extract the ID from the result sets,
    if even for how verbose and imperative it is. Unfortunately there is
    no translation from a HasBOLT node Structure object to a Cypher node
    AFAIK, so we can't sub in a node straight into the next query when
    it's returned from the previous one. ID's seem to be the Right Way to
    do this.
    
    Many of the reusable function definitions are also ugly and ad hoc,
    often unique'd by a filepath that it closes over and with conflicting
    requirements to finalize into a BoltActionT or keep as in the DSL
    (compare the methods in types2cypher vs. ast2cypher).
    
    Finally, one last bit of ugliness is the ordering of queries. Because
    much of the relationship construction is done without much information
    about at least one of the nodes (e.g. in types, we only know the type
    ID), we either have to insert all of the nodes first, or pre-empt with
    an incomplete node and fill in the properties of that later, which
    turns nice MERGE clauses into uglier SET clauses and may leave
    incomplete nodes if we never get to filling them out/if the
    relationship is ever invalid. In types2cypher, I have to iterate over
    the list of types twice explicitly, which is kinda upsetting.
    Initially I wanted to wrap an appropriate monad/transformer to sort the
    queries so the node queries happen first, but the explicit refactor is
    so much more straightforward and my ordering rules wouldn't be that
    elegant anyways (e.g. merge vs. match).

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,260 @@
-module Main where
+{-# LANGUAGE OverloadedLabels, OverloadedStrings, ViewPatterns, NamedFieldPuns, RecordWildCards, TemplateHaskell, LambdaCase, TupleSections, Rank2Types, MultiWayIf #-}
 
+import qualified Data.Set as S
+import Data.Text ( Text(..) )
+import qualified Data.Text as T
+import qualified Data.Array as A
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString.UTF8 as UB
+import qualified Data.ByteString.Lazy.UTF8 as UBL
+import qualified Data.Aeson as Aeson
+
+import Control.Arrow ( (***), (&&&), first, second )
+import Control.Monad ( void )
+import Data.Data ( showConstr, Data(..) )
+import Data.Foldable ( foldrM )
+import System.Environment ( getArgs )
+
+-- GHC
+import SrcLoc ( SrcLoc(..), SrcSpan, RealSrcSpan(..), mkSrcLoc, mkSrcSpan, containsSpan, mkRealSrcSpan, mkRealSrcLoc, realSrcSpanStart, realSrcSpanEnd, srcSpanStartLine, srcSpanStartCol, srcSpanEndLine, srcSpanEndCol )
+import Name ( nameOccName, nameModule_maybe, Name(..) )
+import GHC ( moduleNameString, moduleName, ModuleName(..) )
+import OccName ( occNameString )
+import FastString ( FastString(..), fsLit, unpackFS )
+import Data.Text.IO ( readFile )
+import Text.Read ( readMaybe )
+import SysTools ( initSysTools )
+import DynFlags ( DynFlags, defaultDynFlags )
+import Unique ( Uniquable(..), Unique(..) )
+import IfaceType ( IfaceTyLit(..), IfaceTyCon(..) )
+
+-- prof & HIE
+import qualified GHC.Prof as Prof
+import HieBin
+import HieTypes
+import HieUtils
+
+-- HasBOLT
+import Data.Default ( Default(..) )
+import Database.Bolt ( (=:) )
+import qualified Database.Bolt as Bolt
+-- import Database.Bolt.Extras
+import Database.Bolt.Extras.DSL
+
+-- 
 import Lib
 
+import Debug.Trace ( trace )
+
+querya_ = Bolt.query_ -- . tracethru
+querya = Bolt.query -- . tracethru
+
+tracethru :: Show a => a -> a
+tracethru s = trace (show s) s
+    
+-- ppr_ast_node :: DynFlags -> HieAST a -> String
+-- ppr_ast_node d = ppr_safe d . ((nodeAnnotations &&& M.map (identInfo) . nodeIdentifiers) . nodeInfo &&& nodeSpan)
+
+-- ppr_ast :: DynFlags -> HieAST a -> String
+-- ppr_ast d = uncurry ((++) . (++"\n")) . (ppr_ast_node d &&& unlines . map (unlines . map (' ':) . lines . ppr_ast d) . nodeChildren)
+
+flat_realsrcspan :: RealSrcSpan -> (FastString, Int, Int, Int, Int)
+flat_realsrcspan sp = (
+    srcSpanFile sp
+    , srcSpanStartLine sp
+    , srcSpanStartCol sp
+    , srcSpanEndLine sp
+    , srcSpanEndCol sp
+  )
+
+span2props pre sp = [
+    (pre `T.append` "sp_fn") =: unpackFS (srcSpanFile sp)
+    , (pre `T.append` "sp_l0") =: srcSpanStartLine sp
+    , (pre `T.append` "sp_ch0") =: srcSpanStartCol sp
+    , (pre `T.append` "sp_lf") =: srcSpanEndLine sp
+    , (pre `T.append` "sp_chf") =: srcSpanEndCol sp
+  ]
+
+scope2props pre scope =
+  ((pre `T.append` "scope") =: showConstr (toConstr scope))
+  : (case scope of
+    LocalScope sp -> span2props (pre `T.append` "scope_") sp
+    _ -> [])
+
+ast2cypher :: Bolt.Pipe -> FilePath -> HieAST TypeIndex -> Bolt.BoltActionT IO Int -- IO ()
+ast2cypher neop path (Node {..}) = do
+    let (ann_ctors, ann_tys) = both (map unpackFS) $ unzip $ S.toList $ nodeAnnotations nodeInfo -- S.toList $ S.map (("annotations" =:) . T.pack . UBL.toString . Aeson.encode . both unpackFS)
+        -- (sp_fn, sp_l0, sp_ch0, sp_lf, sp_chf) = flat_realsrcspan nodeSpan
+        sp_props = span2props "" nodeSpan
+    ((!! "id(n)") -> I this_id) <- querya $ formQuery $ do -- TODO URGENT this pattern match is dangerous; make into exception to rollback transaction upon pat match failure
+      mergeF [PS $ P $ (#n .: "GraphieAST" .# (("ann_ctors" =: ann_ctors) : ("ann_tys" =: ann_tys) : sp_props))]
+      _ <- mapM (\(idx, ty) -> do
+          let tynode = withIdentifier (T.pack $ "ty" ++ show idx) defN
+          withF ["n"]
+          matchF [PS $ P $ tynode .: "GraphieTy" .# ["idx" =: ty]]
+          mergeF [PS $ (#n -: (defR .: "GRAPHIE_AST2TY" .# ["pos" =: idx]) :!->: tynode)]
+        ) (zip ([0..] :: [Int]) $ nodeType nodeInfo)
+      _ <- mapM (\(idx, (ident, (IdentifierDetails {..}))) -> do
+          let ident_ident = "ident_" ++ show idx
+              ident_node = withIdentifier (T.pack ident_ident) defN
+              ident_props = case ident of
+                Left name -> ["con" =: T.pack "Name", "name" =: uniquable2text name]
+                Right modname -> ["con" =: T.pack "ModuleName", "name" =: uniquable2text modname]
+          mergeF [PS $ P $ ident_node .: "GraphieIdent" .# ident_props]
+          _ <- mapM (\(ctx_idx, ctx) -> do
+              let ctxnode = withIdentifier (T.pack $ "ctx" ++ (show ctx_idx) ++ "_" ++ ident_ident) defN
+              ctxinfo2cypher ctxnode ctx
+              mergeF [PS $ (#n -: (defR .: "GRAPHIE_AST2CTX") :!->: ctxnode)]
+            ) (zip [0..] $ S.toList identInfo)
+          case identType of
+            Just ty -> do
+              let ident_ty_node = withIdentifier (T.pack $ "ty_" ++ ident_ident) defN
+              withF ["n", T.pack ident_ident]
+              matchF [PS $ P $ ident_node .: "GraphieTy" .# ["idx" =: ty]]
+              mergeF [PS $ ident_node -: (defR .: "GRAPHIE_IDENT2TY") :!->: ident_ty_node]
+            Nothing -> pure ()
+        ) (zip [0..] $ M.assocs (nodeIdentifiers nodeInfo))
+      returnF ["id(n)"] -- hard for me to overstate how important this is
+      
+    next_id <- mapM (ast2cypher neop path) nodeChildren
+    _ <- querya $ formQuery $ do
+      matchF [PS $ P #n]
+      matchF [PS $ P #m]
+      whereF $ (ID "n" this_id) :&&: (ID "m" this_id)
+      mergeF [PS $ #n -: (defR .: "GRAPHIE_AST2AST") :!->: #m]
+      
+    return this_id
+    
+  where -- TODO refine encapsulations
+    mk_ctx_query node con props = mergeF [ -- TODO merge with mk_ty_query, unified by props and label
+      PS $ P $ node
+        .: "GraphieCtx"
+        .# (
+            ("con" =: T.pack con)
+            : ("path" =: T.pack path)
+            : props
+          )
+      ]
+    -- ctxinfo2cypher :: NodeSelector -> ContextInfo -> Free Expr ()
+    ctxinfo2cypher node = \case
+      Use -> mk_ctx_query node "Use" []
+      MatchBind -> mk_ctx_query node "MatchBind" []
+      IEThing ty -> mk_ctx_query node "MatchBind" ["ietycon" =: T.pack (show ty)]
+      TyDecl -> mk_ctx_query node "MatchBind" []
+      ValBind bindty bindscope m_bindspan -> mk_ctx_query node "ValBind" $
+          ["bindtycon" =: T.pack (show bindty)]
+          ++ (case m_bindspan of
+              Just sp -> span2props "bind_" sp
+              Nothing -> [])
+          ++ scope2props "" bindscope
+      PatternBind patscope outscope m_bindspan -> mk_ctx_query node "PatternBind" $
+        (case m_bindspan of
+            Just sp -> span2props "bind_" sp
+            Nothing -> [])
+        ++ scope2props "" patscope
+        ++ scope2props "" outscope
+      ClassTyDecl m_bindspan -> mk_ctx_query node "ClassTyDecl" $ case m_bindspan of
+        Just sp -> span2props "bind_" sp
+        Nothing -> []
+      Decl declty m_bindspan -> mk_ctx_query node "ClassTyDecl" $
+        ["decltycon" =: show declty]
+        ++ (case m_bindspan of
+          Just sp -> span2props "bind_" sp
+          Nothing -> [])
+      TyVarBind declscope tyvarscope -> mk_ctx_query node "PatternBind" $ -- `declscope` as a name is just a guess, not really sure what that first arg is
+        scope2props "" declscope
+        -- TODO the rest, good luck :S
+        -- ++ (case tyvarscope of
+        --   ResolvedScopes scopes -> )
+      RecField ctx m_bindspan -> mk_ctx_query node "RecField" $
+        ["ctxtycon" =: T.pack (show ctx)]
+        ++ (case m_bindspan of
+          Just sp -> span2props "bind_" sp
+          Nothing -> [])
+
+uniquable2text :: Uniquable s => s -> Text
+uniquable2text = T.pack . show . getUnique
+
+types2cypher :: Foldable t => FilePath -> t (TypeIndex, HieTypeFlat) -> Bolt.BoltActionT IO ()
+types2cypher path tys = do
+  -- insert nodes first
+  foldrM (\(idx, ty) () -> case ty of
+      HTyVarTy (uniquable2text -> name) -> mk_ty_query idx "TyVarTy" ["name" =: name]
+      HAppTy _t0 _targs -> mk_ty_query idx "AppTy" []
+      HTyConApp (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) }) _targs -> mk_ty_query idx "TyConApp" ["name" =: name]
+      HForAllTy _ _ -> pure () -- TODO
+      HFunTy tyarg tyret -> mk_ty_query idx "FunTy" []
+      HQualTy tyctx tyret -> mk_ty_query idx "FunTy" []
+      HLitTy tylit -> mk_ty_query idx "HLitTy" [
+        case tylit of
+          IfaceNumTyLit num -> "value" =: num
+          IfaceStrTyLit fs -> "value" =: unpackFS fs
+        ]
+      HCastTy ty -> mk_ty_query idx "CastTy" []
+      HCoercionTy -> mk_ty_query idx "CoercionTy" []
+    ) () tys
+  -- then insert relationships
+  -- TODO maybe make a monad/transformer to sort these into the right order
+  foldrM (\(idx, ty) () -> case ty of
+      HAppTy t0 (HieArgs tn) -> mk_args_query t0 tn
+      HTyConApp _ (HieArgs tn) -> mk_args_query idx tn -- (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) })
+      HForAllTy _ _ -> pure () -- TODO
+      HFunTy tyarg tyret -> do
+        mk_ty_rel idx tyarg ["pos" =: (0 :: Int)]
+        mk_ty_rel idx tyret ["pos" =: (1 :: Int)]
+      HQualTy tyctx tyret -> do
+        mk_ty_rel idx tyctx ["pos" =: (0 :: Int)]
+        mk_ty_rel idx tyret ["pos" =: (1 :: Int)]
+      HCastTy ty -> mk_ty_rel idx ty []
+      _ -> pure ()
+    ) () tys
+  where -- TODO optimize by reusing labelled nodes (return from these query ctors) -- TODO clean up encapsulations (e.g. closure over `path` is a bit arbitrary and loose)
+    mk_ty_query idx con props = querya_ $ formQuery $ mergeF [
+      PS $ P $ defN .: "GraphieTy"
+                .# (
+                    ("con" =: T.pack con)
+                    : ("path" =: T.pack path)
+                    : ("idx" =: idx)
+                    : props
+                  )
+      ]
+    mk_args_query t0 tn = foldrM (\(argpos, (_argvisible, argt)) () -> mk_ty_rel t0 argt ["pos" =: argpos]) () (zip ([0..] :: [Int]) tn) -- LOSS _argvisible
+    mk_ty_rel tya tyb relprops = querya_ $ formQuery $ do
+      matchF [
+        PS $ P $ #a .: "GraphieTy"
+                    .# ["path" =: T.pack path, "idx" =: tya],
+        PS $ P $ #b .: "GraphieTy"
+                    .# ["path" =: T.pack path, "idx" =: tyb]
+        ]
+      mergeF [PS $ (#a -: (defR .: "GRAPHIE_TY_REL" .# relprops) :!->: (#b))]
+
+hiefile_props :: HieFile -> [(Text, Bolt.Value)]
+hiefile_props (HieFile {..}) = [
+    "hs_file" =: T.pack hie_hs_file
+    , "hie_module" =: uniquable2text hie_module
+  ]
+
 main :: IO ()
-main = someFunc
+main = do
+  dflags <- dynFlagsForPrinting
+  nc <- makeNc
+  neop <- Bolt.connect def {
+      Bolt.user = "neo4j",
+      Bolt.password = "postgres"
+      -- secure = True
+    }
+  dhies <- getArgs -- TODO switch to glob-based
+  fhies <- fmap concat $ mapM getHieFilesIn $ dhies
+  _ <- foldrM (\path nc' -> do
+      (hie_file_result -> hie, nc'') <- readHieFile nc' path
+      Bolt.run neop $ Bolt.transact $ do
+        -- base file
+        querya $ formQuery $ mergeF [PS $ P $ defN .: "GraphieHieFile" .# hiefile_props hie]
+        -- types
+        types2cypher path (A.assocs $ hie_types hie)
+        -- TODO exported names
+        -- i.e. exports2cypher (hie_exports hie)
+        foldrM (\(fs_name, ast) () -> ast2cypher neop (unpackFS fs_name) ast) () (M.assocs $ getAsts $ hie_asts hie)
+      return nc''
+    ) nc fhies
+  pure ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,42 @@
 module Lib
-    ( someFunc
+    ( both, dynFlagsForPrinting, makeNc, getHieFilesIn
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- System
+import System.FilePath
+import System.Directory
+
+import SysTools ( initSysTools )
+import DynFlags ( DynFlags, defaultDynFlags )
+import GHC.Paths (libdir)
+import NameCache ( NameCache(..), initNameCache )
+import UniqSupply ( mkSplitUniqSupply )
+
+dynFlagsForPrinting :: IO DynFlags
+dynFlagsForPrinting = do
+  systemSettings <- initSysTools libdir
+  return $ defaultDynFlags systemSettings ([], [])
+  
+makeNc :: IO NameCache
+makeNc = do
+  uniq_supply <- mkSplitUniqSupply 'z'
+  return $ initNameCache uniq_supply []
+
+getHieFilesIn :: FilePath -> IO [FilePath]
+getHieFilesIn path = do
+  exists <- doesPathExist path
+  if exists then do
+    isFile <- doesFileExist path
+    isDir <- doesDirectoryExist path
+    if isFile && ("hie" `isExtensionOf` path) then do
+      path' <- canonicalizePath path
+      fsize <- getFileSize path'
+      if fsize == 0 then mempty else return [path']
+    else if isDir then do
+      cnts <- listDirectory path
+      withCurrentDirectory path $ foldMap getHieFilesIn cnts
+    else return []
+  else
+    return []
+    
+both f (a, b) = (f a, f b)
