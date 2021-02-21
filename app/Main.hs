d2ce81e54548ad7a38741a4f8a0358edcaf67a93
Derek Lam <dc.lam@mail.utoronto.ca>
2021-02-20 18:34:46 -0800

    Separate writing nodes & rels + remap type indices
    The big changes the last commit hinted at have been implemented (without
    testing):
    
    - Nodes and rels have separate insertion functions, since nodes need IDs
      and rels need to not have IDs
    - Type IDs have been remapped per file using a more global state that
      wraps the pre-existing `ASTState`. We're keeping them separate because
      it's proving a nice separation between the more core bean-counting (ID
      and file handle) code and the less agnostic HIE-facing code above.
    - Files are now generated both per label _and_ per schema. This is
      because some parameters are optional and some nodes do/don't have them
      which screws up the CSV (whose headers are built based on the first
      node to arrive).

--- app/Main.hs
+++ app/Main.hs
@@ -19,8 +19,10 @@ import Data.Char ( toLower, isAlphaNum, ord )
 import Control.Arrow ( (***), (&&&), first, second )
 import Control.Exception ( Exception(..), throw )
 import Control.Applicative ( (<|>), Alternative(..) )
-import Control.Monad ( void, zipWithM )
+import Control.Monad ( void, zipWithM, join )
 import Data.List ( partition )
+import Data.Maybe ( fromMaybe )
+import Data.Bitraversable ( bisequence )
 import Data.Data ( showConstr, Data(..) )
 import Data.Foldable ( foldrM )
 import System.Environment ( getArgs )
@@ -28,7 +30,7 @@ import Control.Lens ( makeLenses )
 import qualified Control.Lens as L
 import Control.Lens.Operators
 import Control.Monad.State.Strict ( MonadState(..), gets, lift )
-import Control.Monad.Trans.State.Strict ( StateT(..), execState, evalState )
+import Control.Monad.Trans.State.Strict ( StateT(..), execStateT, evalStateT, runStateT )
 
 -- GHC
 import SrcLoc ( SrcLoc(..), SrcSpan, RealSrcSpan(..), mkSrcLoc, mkSrcSpan, containsSpan, mkRealSrcSpan, mkRealSrcLoc, realSrcSpanStart, realSrcSpanEnd, srcSpanStartLine, srcSpanStartCol, srcSpanEndLine, srcSpanEndCol )
@@ -69,11 +71,21 @@ import Debug.Trace ( trace )
 tracethru :: Show a => a -> a
 tracethru s = trace (show s) s
 
+data EntityType = Neo4JNode | Neo4JRelationship
+-- core state, counting node IDs and managing file handles
 data ASTState = ASTState {
   _ast_nodeid :: Int
-  , _ast_keys :: M.Map T.Text Handle -- label -> node file
+  , _ast_keys :: M.Map T.Text (EntityType, M.Map (S.Set T.Text) Handle) -- label -> (entity type, params) -> node file; the second map is because some nodes have optional params that others omitted
 }
 makeLenses ''ASTState
+
+-- outer state, including HIE-specific things like type index mapping
+data TyASTState = TyASTState {
+  _tyast_ty_idx_map :: M.Map (T.Text, Int) Int -- path + ty idx -> neo4j idx
+  , _tyast_aststate :: ASTState
+}
+makeLenses ''TyASTState
+
 data Field = I Int | F Float | S T.Text
 instance CSV.ToField Field where
   toField = \case
@@ -112,7 +124,7 @@ instance {-# OVERLAPPABLE #-} Fieldable f => Fieldable [f] where
         CSV.encDelimiter = fromIntegral $ ord '|'
       }
 
-instance Fieldable Text where
+instance Fieldable T.Text where
   mkField = S
 
 field2neo4j_ty :: Field -> T.Text
@@ -121,60 +133,71 @@ field2neo4j_ty = \case
   F _ -> ":FLOAT"
   S _ -> ":STRING"
 
-modifyM :: MonadState s m => (s -> m s) -> m ()
-modifyM f = do
-  old <- get
+-- thanks https://stackoverflow.com/a/46113439/3925507
+modifyingM :: MonadState s m => L.Lens' s a -> (a -> m a) -> m ()
+modifyingM l f = do
+  old <- L.use l
   new <- f old
-  put new
+  l .= new
+
+-- modifyM :: MonadState s m => (s -> m s) -> m ()
+-- modifyM f = do
+--   old <- get
+--   new <- f old
+--   put new
 
 type ASTStateT a = StateT ASTState IO a
-ast_node :: T.Text -> [(T.Text, Field)] -> ASTStateT Int
-ast_node label params_raw = do
-  -- TODO rels don't have IDs
-  -- TODO alternatively, partition this function into common parts and make node + rel separate functions (since :ID has special treatment but :START_ID and :END_ID don't)
-  -- I do have to make it distinct, in the state too, because the files need to be distinguished
-  
-  let (magic_params, params) =
-        both M.fromList
-        $ first (map (first (magic_keys M.!)))
-        $ partition ((`M.member` magic_keys) . fst) params_raw
-      magic_keys :: M.Map T.Text T.Text
-      magic_keys = M.fromList [
-          ("idx", ":ID")
-          , ("start_˙id", ":START_ID")
-          , ("end_id", ":END_ID")
-        ]
-  
-  -- NOTE this isn't lazy in incrementing, it always increments. That's kind of okay for now, but is truly a bit wasteful... the alternative is a case statement or MaybeT
-  -- magic_params' <- M.alterF (\mx -> runMaybeT $ do
-  --     MaybeT $ pure mx -- no wait i want the opposite of this
-  --     lift $ I <$> (ast_nodeid <<%= (+1))
-  --   ) ":ID" magic_params
-  magic_params' <- M.alterF (\mx -> case mx of
-      Just x -> pure mx
-      Nothing -> (Just . I) <$> (ast_nodeid <<%= (+1))
-    ) ":ID" magic_params
-    
-  let new_file = do
-        h' <- lift (openFile ("tmp/" ++ filter isAlphaNum (T.unpack label)) WriteMode)
+type TyASTStateT a = StateT TyASTState IO a
+
+-- magic params are labelled with their keys directly, rather than their types. They're always nameless (subject to change?)
+write_entity :: EntityType -> T.Text -> M.Map T.Text Field -> M.Map T.Text Field -> ASTStateT ()
+write_entity entity_ty label magic_params params = do
+  let all_params = M.keysSet magic_params `S.union` M.keysSet params
+      m_new_file = do
+        idx <- gets (fromMaybe 0 . fmap (M.size . snd) . (M.!? label) . (^. ast_keys)) -- super sketchy. We need to 
+        let fname = "tmp/" ++ filter isAlphaNum (T.unpack label) ++ (show idx) ++ ".csv"
+            header =
+              M.keys magic_params
+              ++ map (uncurry T.append . second field2neo4j_ty) (M.toAscList params)
+              -- NOTE this is a different order from `all_params`, which UNIONS the sets together. DO NOT EVER USE IT FOR WRITING HEADERS.
+              
+        handle <- lift $ openFile fname WriteMode
         -- write header
-        lift $ BL.hPut h' $ CSV.encode $ pure $ 
-          ":LABEL"
-          : M.keys magic_params'
-          ++ map (uncurry T.append . second field2neo4j_ty) (M.toAscList params)
-        return h'
-  modifyM (ast_keys %%~ M.alterF (\mv -> case mv of
-      Just v -> pure mv
-      Nothing -> Just <$> new_file
-    ) label) -- man what a minefield. I just want to take the state and return a transformation done in the corresponding monad to that state, in a compact and idiomatic way. The `ast_keys %%~` is the right way to modify the state to produce an `m s`, but this is not exactly a lens
-        -- %= (M.insert label h')
-  ksets' <- gets (^. ast_keys)
-  lift $ BL.hPut (ksets' M.! label) $ CSV.encode $ pure $
-    (S label)
-    : M.elems magic_params'
-    ++ M.elems params -- assumes the list keys for all labels are identical, TODO make this more robust rather than this implicit equality (although it requires buffering the whole set of entries in case the keys change, or one file per label-keyset combination)
-  return $ (\(I i) -> i) $ magic_params' M.! ":ID" -- meeeeeh.
+        lift $ BL.hPut handle $ CSV.encode [header]
+        return handle
+      row =
+        -- S label :
+        M.elems magic_params
+        ++ M.elems params
   
+  -- i'm sure there's a nice way to both set and return the set value using lenses, but i can't be bothered right now, so we'll set blindly then get
+  modifyingM ast_keys $ M.alterF (\mv -> Just <$> case mv of
+      Just v -> (bisequence . ($ v) . (pure ***)) $ M.alterF (\mv' -> case mv' of
+          Just _v' -> pure mv'
+          Nothing -> Just <$> m_new_file
+        ) all_params
+      Nothing -> ((entity_ty, ) . M.singleton all_params) <$> m_new_file
+    ) label
+  handle <- ((M.! all_params) . snd . (M.! label)) <$> gets (^. ast_keys)
+  void $ lift $ BL.hPut handle $ CSV.encode [row]
+
+write_node :: T.Text -> [(T.Text, Field)] -> ASTStateT Int
+write_node label params_raw = do
+  idx <- ast_nodeid <<%= (+1) -- postfix ++
+  let params = M.fromList params_raw
+      magic_params = M.fromList [ ":ID" =: idx ]
+  write_entity Neo4JNode label magic_params params
+  pure idx
+
+write_rel :: T.Text -> Int -> Int -> [(T.Text, Field)] -> ASTStateT ()
+write_rel label from_idx to_idx params_raw = do
+  idx <- ast_nodeid <<%= (+1) -- postfix ++
+  let params = M.fromList params_raw
+      magic_params = M.fromList [
+          ":START_ID" =: from_idx
+          , ":END_ID" =: to_idx
+        ]
+  write_entity Neo4JRelationship label magic_params params
 
 -- ppr_ast_node :: DynFlags -> HieAST a -> String
 -- ppr_ast_node d = ppr_safe d . ((nodeAnnotations &&& M.map (identInfo) . nodeIdentifiers) . nodeInfo &&& nodeSpan)
@@ -214,64 +237,63 @@ scope2props pre scope =
 --     rec:_ -> rec `Bolt.at` ident'
 --     _ -> throw $ Bolt.RecordHasNoKey ident'
 
-ast2cypher :: FilePath -> FilePath -> HieAST TypeIndex -> ASTStateT Int -- Bolt.BoltActionT IO Int -- IO ()
+-- is there a nicer, lensical way of doing this?
+nest :: Monad m => L.Lens' s a -> StateT a m c -> StateT s m c
+nest lout min = do
+  sout <- gets (^. lout)
+  (c, sout') <- lift $ runStateT min sout
+  lout .= sout'
+  pure c
+
+ast2cypher :: FilePath -> FilePath -> HieAST TypeIndex ->TyASTStateT Int -- Bolt.BoltActionT IO Int -- IO ()
 ast2cypher hs_path hie_path (Node {..}) = do
     let (ann_ctors, ann_tys) = both (map unpackFS) $ unzip $ S.toList $ nodeAnnotations nodeInfo -- S.toList $ S.map (("annotations" =:) . T.pack . UBL.toString . Aeson.encode . both unpackFS)
         -- (sp_fn, sp_l0, sp_ch0, sp_lf, sp_chf) = flat_realsrcspan nodeSpan
         sp_props = span2props "" nodeSpan
-    ast_idx <- ast_node "GraphieAST" (
+        
+    tymap <- gets (^. tyast_ty_idx_map)
+    let cvt x = fromMaybe (error $ show (hie_path, x, M.size tymap)) . (tymap M.!?) . (T.pack hie_path,) $ x
+    ast_idx <- nest tyast_aststate $ write_node "GraphieAST" (
         ("ann_ctors" =: ann_ctors)
         : ("ann_tys" =: ann_tys)
         : sp_props
       )
     
     -- type relationships
-    _ <- mapM (\(idx, ty) -> do
-        ty_idx <- ast_node "GraphieTy" [
-            "idx" =: ty
-            , "path" =: T.pack hie_path
-          ]
-        _ty_rel <- ast_node "GRAPHIE_AST2TY" [
-            "pos" =: idx,
-            "start_id" =: ast_idx,
-            "end_id" =: ty_idx
-          ]
-        pure ()
-      ) (zip ([0..] :: [Int]) $ nodeType nodeInfo)
+    nest tyast_aststate $ do
+      mapM (\(pos, ty_idx) ->
+          write_rel "GRAPHIE_AST2TY" ast_idx (cvt ty_idx) [
+                "pos" =: pos
+              ]
+        ) (zip ([0..] :: [Int]) $ nodeType nodeInfo)
     
-    -- context relationships and misc
-    _ <- mapM (\(ident, (IdentifierDetails {..})) -> do
-        let ident_props = case ident of
-              Left name -> ["con" =: T.pack "Name", "name" =: uniquable2text name]
-              Right modname -> ["con" =: T.pack "ModuleName", "name" =: uniquable2text modname]
-        
-        ident_idx <- ast_node "GraphieIdent" ident_props
-        
-        _ <- mapM (\ctx -> do
-            ctx_idx <- ctxinfo2cypher ctx
-            ast_node "GRAPHIE_AST2CTX" [
-                "start_id" =: ast_idx
-                , "end_id" =: ctx_idx
-              ]
-          ) (S.toList identInfo)
-        
-        case identType of
-          Just ty -> do
-            ident_ty_idx <- ast_node "GraphieTy" ["idx" =: ty, "path" =: T.pack hie_path]
-            void $ ast_node "GRAPHIE_IDENT2TY" [
-                "start_id" =: ident_idx
-                , "end_id" =: ident_ty_idx
-              ]
-          Nothing -> pure ()
-      ) (M.assocs (nodeIdentifiers nodeInfo))
+      -- context relationships and misc
+      mapM (\(ident, (IdentifierDetails {..})) -> do
+          let ident_props = case ident of
+                Left name -> ["con" =: T.pack "Name", "name" =: uniquable2text name]
+                Right modname -> ["con" =: T.pack "ModuleName", "name" =: uniquable2text modname]
+          
+          ident_idx <- write_node "GraphieIdent" ident_props
+          
+          _ <- mapM (\ctx -> do
+              ctx_idx <- ctxinfo2cypher ctx
+              write_rel "GRAPHIE_AST2CTX" ast_idx ctx_idx []
+            ) (S.toList identInfo)
+          
+          case identType of
+            Just ty_idx -> do
+              void $ write_rel "GRAPHIE_IDENT2TY" ident_idx (cvt ty_idx) []
+            Nothing -> pure ()
+        ) (M.assocs (nodeIdentifiers nodeInfo))
     
     next_idxs <- mapM (ast2cypher hs_path hie_path) nodeChildren
-    _ <- (flip (`zipWithM` ([0..] :: [Int])) next_idxs) $ \pos next_idx -> ast_node "GRAPHIE_AST2AST" ["pos" =: pos, "start_id" =: ast_idx, "end_id" =: next_idx]
+      
+    nest tyast_aststate $ (flip (`zipWithM` ([0..] :: [Int])) next_idxs) $ \pos next_idx -> write_rel "GRAPHIE_AST2AST" ast_idx next_idx ["pos" =: pos]
       
     return ast_idx
     
   where -- TODO refine encapsulations
-    mk_ctx_query con props = ast_node "GraphicCtx" (
+    mk_ctx_query con props = write_node "GraphieCtx" (
         ("con" =: T.pack con)
         : ("path" =: T.pack hie_path)
         : props
@@ -319,10 +341,10 @@ uniquable2text = T.pack . show . getUnique
 datactor2text :: Data d => d -> Text
 datactor2text = T.pack . showConstr . toConstr 
 
-types2cypher :: Traversable t => FilePath -> t (TypeIndex, HieTypeFlat) -> ASTStateT () -- Bolt.BoltActionT IO ()
+types2cypher :: FilePath -> [(TypeIndex, HieTypeFlat)] -> TyASTStateT () -- Bolt.BoltActionT IO ()
 types2cypher path tys = do
   -- insert nodes first
-  mapM (\(idx, ty) -> case ty of
+  (M.fromList -> tymap) <- nest tyast_aststate $ mapM (\(idx, ty) -> case ty of
       HTyVarTy (uniquable2text -> name) -> mk_ty_query idx "TyVarTy" ["name" =: name]
       HAppTy _t0 _targs -> mk_ty_query idx "AppTy" []
       HTyConApp (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) }) _targs -> mk_ty_query idx "TyConApp" ["name" =: name]
@@ -337,36 +359,37 @@ types2cypher path tys = do
       HCastTy ty -> mk_ty_query idx "CastTy" []
       HCoercionTy -> mk_ty_query idx "CoercionTy" []
     ) tys
-  -- then insert relationships
+  
+  -- might not be necessary to reference tymap' because all the types should be confined to this path (right?)
+  tymap' <- tyast_ty_idx_map <%= (M.union tymap)
+  let cvt = (tymap' M.!) . (T.pack path,) -- tempting to build this into the mk_* functions and move them into the `do` body...
+  
+  --  insert relationships last
   -- TODO maybe make a monad/transformer to sort these into the right order
-  void $ mapM (\(idx, ty) -> case ty of
-      HAppTy t0 (HieArgs tn) -> mk_args_query t0 tn
-      HTyConApp _ (HieArgs tn) -> mk_args_query idx tn -- (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) })
-      HForAllTy ((name, ty0), argflag) ty1 -> mk_ty_rel ty0 ty1 ["name" =: uniquable2text name, "argflag" =: datactor2text argflag]
+  void $ nest tyast_aststate $ mapM (\(idx, ty) -> case ty of
+      HAppTy t0 (HieArgs tn) -> mk_args_query (cvt t0) (map (second cvt) tn)
+      HTyConApp _ (HieArgs tn) -> mk_args_query (cvt idx) (map (second cvt) tn) -- (IfaceTyCon { ifaceTyConName = (uniquable2text -> name) })
+      HForAllTy ((name, ty0), argflag) ty1 -> mk_ty_rel (cvt ty0) (cvt ty1) ["name" =: uniquable2text name, "argflag" =: datactor2text argflag]
       HFunTy tyarg tyret -> do
-        mk_ty_rel idx tyarg ["pos" =: (0 :: Int)]
-        mk_ty_rel idx tyret ["pos" =: (1 :: Int)]
+        mk_ty_rel (cvt idx) (cvt tyarg) ["pos" =: (0 :: Int)]
+        mk_ty_rel (cvt idx) (cvt tyret) ["pos" =: (1 :: Int)]
       HQualTy tyctx tyret -> do
-        mk_ty_rel idx tyctx ["pos" =: (0 :: Int)]
-        mk_ty_rel idx tyret ["pos" =: (1 :: Int)]
-      HCastTy ty -> mk_ty_rel idx ty []
+        mk_ty_rel (cvt idx) (cvt tyctx) ["pos" =: (0 :: Int)]
+        mk_ty_rel (cvt idx) (cvt tyret) ["pos" =: (1 :: Int)]
+      HCastTy ty -> mk_ty_rel (cvt idx) (cvt ty) []
       _ -> pure ()
     ) tys
   where -- TODO optimize by reusing labelled nodes (return from these query ctors) -- TODO clean up encapsulations (e.g. closure over `path` is a bit arbitrary and loose)
-    mk_ty_query idx con props = ast_node "GraphieTy" (
+    mk_ty_query idx con props = write_node "GraphieTy" (
         ("con" =: T.pack con)
         : ("path" =: T.pack path)
         : ("idx" =: idx)
         : props
       )
+      <&> ((T.pack path, idx), )
     mk_args_query t0 tn = void $ mapM (\(argpos, (_argvisible, argt)) -> mk_ty_rel t0 argt ["pos" =: argpos]) (zip ([0..] :: [Int]) tn) -- LOSS _argvisible
     -- TODO URGENT path used to be used to discriminate between type vars of differing paths, this needs to be re-introduced as a query against the state. All the IDs need to be remapped... damn this is a pain.
-    mk_ty_rel tya_idx tyb_idx relprops = void $ ast_node "GRAPHIE_TY_REL" (
-          "path" =: T.pack path
-          : "start_id" =: tya_idx
-          : "end_id" =: tyb_idx
-          : relprops
-        )
+    mk_ty_rel tya_idx tyb_idx relprops = void $ write_rel "GRAPHIE_TY_REL" tya_idx tyb_idx (("path" =: T.pack path) : relprops)
       -- -- make the relationship undirected:
       -- ast_node "GRAPHIE_TY_REL" (
       --     "path" =: T.pack path
@@ -389,25 +412,23 @@ main = do
   do
     dhies <- getArgs -- TODO switch to glob-based
     fhies <- fmap concat $ mapM getHieFilesIn $ dhies
-    _ <- foldrM (\path nc' -> do
-        (hie_file_result -> hie, nc'') <- readHieFile nc' path
-        putStrLn path
+    
+    ((^. (tyast_aststate . ast_keys)) -> ks) <- (`execStateT` (TyASTState mempty (ASTState 0 mempty))) $ foldrM (\path nc' -> do
+        (hie_file_result -> hie, nc'') <- lift $ readHieFile nc' path
+        lift $ putStrLn path
+      
+        -- base file
+        hie_file_idx <- nest tyast_aststate $ write_node "GraphieHieFile" (hiefile_props hie)
+        -- types
+        -- lift $ print $ map fst $ (A.assocs $ hie_types hie)
+        types2cypher path (A.assocs $ hie_types hie)
+        -- TODO exported names
+        -- i.e. exports2cypher (hie_exports hie)
+        -- ASTs
+        ast_ids <- mapM (\(fs_name, ast) -> ast2cypher (unpackFS fs_name) path ast) (M.assocs $ getAsts $ hie_asts hie)
         
-        (`runStateT` (ASTState 0 mempty)) $ do
-          -- base file
-          hie_file_idx <- ast_node "GraphieHieFile" (hiefile_props hie)
-          -- types
-          types2cypher path (A.assocs $ hie_types hie)
-          -- TODO exported names
-          -- i.e. exports2cypher (hie_exports hie)
-          -- ASTs
-          ast_ids <- mapM (\(fs_name, ast) -> ast2cypher (unpackFS fs_name) path ast) (M.assocs $ getAsts $ hie_asts hie)
-          
-          (`mapM` ast_ids) $ \ast_idx -> 
-            ast_node "GRAPHIE_FILE2AST" [
-                "start_id" =: hie_file_idx
-                , "end_id" =: ast_idx 
-              ]
+        nest tyast_aststate $ (`mapM` ast_ids) $ \ast_idx -> 
+          write_rel "GRAPHIE_FILE2AST" hie_file_idx ast_idx []
         return nc''
       ) nc fhies
-    pure ()
+    void $ mapM hClose $ concatMap (M.elems . snd) $ M.elems $ ks
